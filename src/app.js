31bbce52d60065ebb36b93751858b72fcb3d2d82
Orealba <orealbas@gmail.com>
2022-01-17 16:03:25 +0000

    arreglos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+let dataset = [who, action, what, when];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// * Aux functions
+const RandomIndex = arr => Math.floor(Math.random() * arr.length);
+const RandomItem = (arr, i) => arr[i];
+const GetString = arr => `${RandomItem(arr, RandomIndex(arr))} `;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const excuseGenerator = arr => {
+  let str = "";
+  for (let v of arr) {
+    str += GetString(v);
+  }
+  return str;
 };
+
+console.log(excuseGenerator(dataset));
